9e61f6e758d4ba1d63b9d0e26c28ed5220227c74
Marcus Jolmes <marcusjolmes191@gmail.co.uk>
2022-05-05 10:33:49 +0100

    Added notes

--- python_work/activity1.py
+++ python_work/activity1.py
@@ -1,11 +1,23 @@
-welcome = "Welcome to code nation!"
+welcome = "Welcome to code nation."
 
 result = (len(welcome))
 
 print(result)
 def check():
     if result %2 == 0:
-        print(welcome,result,"Even number!")
+        print(welcome,result,"is an even number!")
     else:
-        print("An odd number!")
+        print(welcome,result,"is an odd number!")
 check()
+
+# welcome = "Welcome to code nation."
+
+# result=len(welcome))
+
+# print(result)
+# def check():
+#     if result %2 == 0:
+#         print(f"The length of the string \"{welcome}\" is {result} and it is even")
+#     else:
+#         print(f"The length of the string \"{welcome}\" is {result} and it is even")
+# check()
